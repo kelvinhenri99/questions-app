cd6ec47ba71867c206a71a88dcbfb9ca889690d0
kelvinhenri99 <kelvin5henri@gmail.com>
2023-06-25 22:03:38 -0300

    feat: create layout questions screen

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -3,4 +3,24 @@
 
 @section('content')
 
+<div class="question">
+  <h1>Nível: <span>NORMAL</span></h1>
+  <div class="questionContainer">
+    <h1>COCA-COLA vs PEPSI</h1>
+    <p>Subtitulo</p>
+    <img src="../images/questions/coca-cola.png">
+    <img src="../images/questions/pepsi.jpg">
+    <h2>Qual você prefere?</h2>
+    <div class="respond">
+      <form action="">
+        <input type="submit" value="Resposta 1">
+      </form>
+      <form action="">
+        <input type="submit" value="Resposta 2">
+      </form>
+    </div>
+  </div>
+
+</div>
+
 @endsection
