bdc1bc4efed0ccc838692dc172fe98377520c526
kelvinhenri99 <kelvin5henri@gmail.com>
2023-06-25 22:04:51 -0300

    fix: question Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuestionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,5 +10,5 @@ Route::get('/', function () {
 
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/question', [AuthController::class, 'question'])->name('question');
+Route::get('/question', [QuestionController::class, 'question'])->name('question');
 Route::get('/analysis', [AuthController::class, 'analysis'])->name('analysis');
