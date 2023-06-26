0426b0c9c77477773031736deb9416e3b500061f
kelvinhenri99 <kelvin5henri@gmail.com>
2023-06-25 22:03:47 -0300

    feat: create table questions

---
+++ database/migrations/2023_06_24_224431_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('describe');
+            $table->string('image1');
+            $table->string('image2');
+            $table->string('level');
+            $table->string('optionOne');
+            $table->string('optionTwo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
